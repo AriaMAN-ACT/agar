d508e8bc6a07ee77810c2ec37d235de02473c299
WolfMAN <aria.azadi.pour@protonmail.com>
2020-05-01 20:05:41 +0430

    do some ui things

--- public/scripts/uiScript.js
+++ public/scripts/uiScript.js
@@ -1,4 +1,26 @@
-let wHeight = $(window).height;
-let wWidth = $(window).width;
+const player = {};
 
-let canvas = document.getElementById('the-canvas');
+const wWidth = $(window).width;
+const wHeight = $(window).height;
+
+const canvas = document.querySelector('#the-canvas');
+const context = canvas.getContext('2d');
+canvas.width = wWidth;
+canvas.height = wHeight;
+
+$(window).load(() => {
+    $('#loginModal').modal('show');
+});
+
+$('.name-form').submit(event => {
+    event.preventDefault();
+    player.username = document.querySelector('#name-input').value;
+    $('#loginModal').modal('hide');
+    $('#spawnModal').modal('show');
+    document.querySelector('.player-name').innerHTML = player.username;
+});
+
+$('.start-game').click(event => {
+    $('.modal').modal('hide');
+    $('.hiddenOnStart').removeAttr('hidden');
+});
